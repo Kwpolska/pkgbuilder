45aaa2b396ec99ebea962a28dcde2c379b3d296a
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-06-28 18:25:46 +0200

    verbosepkglists displays an error when the terminal is too small (just like in pacman)
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

--- pkgbuilder/upgrade.py
+++ pkgbuilder/upgrade.py
@@ -237,6 +237,9 @@ def auto_upgrade(downgrade=False, vcsup=False):
                     verbosepkglists = False
                     DS.log.warning('VerbosePkgLists disabled, terminal is '
                                    'not wide enough')
+                    # string stolen from pacman
+                    print(_('warning: insufficient columns available for '
+                            'table display'))
                 else:
                     print('\n{0}\n'.format(targetstring.strip()))
                     print(fstring.format(i=headers))
