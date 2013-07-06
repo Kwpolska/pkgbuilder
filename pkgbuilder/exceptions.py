54e636461d8f92a35956c808a4cb2c01dbc5457e
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-07-06 13:51:01 +0200

    v3.1.3: Fixes to the safeupgrade, added Italian, Spanish and Turkish.
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

--- pkgbuilder/exceptions.py
+++ pkgbuilder/exceptions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
-# PKGBUILDer v3.1.2
+# PKGBUILDer v3.1.3
 # An AUR helper (and library) in Python 3.
 # Copyright © 2011-2013, Kwpolska.
 # See /LICENSE for licensing information.
