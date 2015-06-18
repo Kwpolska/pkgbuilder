22b0815f5f4b9e76640087ea528867546a778b69
Chris Warrick <kwpolska@gmail.com>
2015-06-18 17:18:48 +0200

    drop |release|
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011-2015, Chris Warrick'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.5.0'
 # The full version, including alpha/beta/rc tags.
 release = '3.5.0'
 
