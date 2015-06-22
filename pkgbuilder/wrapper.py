5df275bcd6aa108a8eb6d3e6b41f2b28e68cc2e5
Chris Warrick <kwpolska@gmail.com>
2015-06-22 18:41:45 +0200

    v3.5.1: fix AURv4 support (use URLPath)
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- pkgbuilder/wrapper.py
+++ pkgbuilder/wrapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 # PBWrapper v0.2.3
-# PKGBUILDer v3.5.0
+# PKGBUILDer v3.5.1
 # An AUR helper (and library) in Python 3.
 # Copyright © 2011-2015, Chris Warrick.
 # See /LICENSE for licensing information.
